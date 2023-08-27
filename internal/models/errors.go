329dfdc797c4bbc15dcd436f046a7aa7cd5b364c
Al <nukassel@xakep.ru>
2023-08-28 02:07:59 +0300

    refactoring

--- internal/models/errors.go
+++ internal/models/errors.go
@@ -5,5 +5,4 @@ import "errors"
 var (
 	ErrShortyNotFound = errors.New("shorty not found")
 	ErrShortyExists   = errors.New("shorty already exist")
-	ErrVisitsNotFound = errors.New("visits not found")
 )
